c7bbb04237849d285ac749750eaf6f81ba3de489
cheerfun <qingyafan@163.com>
2019-09-06 23:58:30 +0800

    feature: add system params, but not test yet

--- cluster.conf.sh
+++ cluster.conf.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-set -exo pipefail
-
-export ALL_SERVER_IPS=("192.168.31.81" "192.168.31.82" "192.168.31.83")
-export MASTER_IP=192.168.31.81
-export NODE_IPS=("192.168.31.81" "192.168.31.82" "192.168.31.83")
+export ALL_SERVER_IPS=("10.0.10.81" "10.0.10.82" "10.0.10.83")
+export MASTER_IP=10.0.10.81
+export NODE_IPS=("10.0.10.81" "10.0.10.82" "10.0.10.83")
 export DOCKER_LOCATION=/home/docker
-export SERVICE_UNIT_LOCATION=/usr/lib/systemd/system
--- cluster.install.sh
+++ cluster.install.sh
@@ -4,6 +4,9 @@ set -exo pipefail
 
 BASE_PATH="$(pwd)"
 USER=root
+ETCD_IPS=("${ALL_SERVER_IPS[@]:0:3}")
+ETCD_ENDPOINTS="https://${ALL_SERVER_IPS[0]}:2379,https://${ALL_SERVER_IPS[1]}:2379,https://${ALL_SERVER_IPS[2]}:2379"
+ETCD_NODES="etcd-node0=https://${ALL_SERVER_IPS[0]}:2380,etcd-node1=https://${ALL_SERVER_IPS[1]}:2380,etcd-node2=https://${ALL_SERVER_IPS[2]}:2380"
 
 # shellcheck disable=SC1091
 . ./cluster.conf.sh
@@ -15,6 +18,21 @@ rm -rf tmp && mkdir tmp
 # stopFirewall shutdown the firewall
 function stopFirewall {
   ssh "$1@$2" "systemctl stop firewalld && systemctl disable firewalld && setenforce 0 | true && sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config"
+}
+
+# shutdownSWAP shot down swap
+function shutdownSWAP {
+  local nodeIP=$1
+  scp ./utils/shutdown_swap.sh "${USER}@${nodeIP}:~/"
+  ssh "${USER}@${nodeIP}" "bash ~/shutdown_swap.sh"
+}
+
+# installSSLTools install ssl tools
+function installSSLTools {
+  chmod +x ./bins/cfssl*
+  cp -rf ./bins/cfssl_linux-amd64 /usr/local/bin/cfssl
+  cp -rf ./bins/cfssl-certinfo_linux-amd64 /usr/local/bin/cfssl-certinfo
+  cp -rf ./bins/cfssljson_linux-amd64 /usr/local/bin/cfssljson
 }
 
 # generateCerts generate the certs
@@ -124,211 +142,205 @@ function installKubernetsNode {
   ssh "${USER}@${nodeIP}" "mkdir -p /var/lib/kubelet && systemctl daemon-reload && systemctl enable kubelet && systemctl enable kube-proxy && systemctl start kubelet && systemctl start kube-proxy"
 }
 
-
-# config master can access node without auth
-ssh-keygen
-for node in "${ALL_SERVER_IPS[@]}"; do
-  ssh-copy-id -i ~/.ssh/id_rsa.pub "${node}"
-  stopFirewall "${USER}" "$node"
-done
-
-
-# shutdown swap in all node
-chmod +x ./utils/shutdown_swap.sh
-for node in "${ALL_SERVER_IPS[@]}"; do
-  scp ./utils/shutdown_swap.sh "${USER}@${node}:~/"
-  ssh "${USER}@${node}" "bash ~/shutdown_swap.sh"
-done
+function main {
+  # config master can access node without auth
+  # and stop firewall
+  # and shutdown swap in all node
+  ssh-keygen
+  chmod +x ./utils/shutdown_swap.sh
+  for node in "${ALL_SERVER_IPS[@]}"; do
+    ssh-copy-id -i ~/.ssh/id_rsa.pub "${node}"
+    stopFirewall "${USER}" "$node"
+    shutdownSWAP "$node"
+  done
 
 
-# install CFSSL tools
-chmod +x ./bins/cfssl*
-cp -rf ./bins/cfssl_linux-amd64 /usr/local/bin/cfssl
-cp -rf ./bins/cfssl-certinfo_linux-amd64 /usr/local/bin/cfssl-certinfo
-cp -rf ./bins/cfssljson_linux-amd64 /usr/local/bin/cfssljson
-
-generateCerts
+  # install ssl tools and generate certs
+  installSSLTools
+  generateCerts
 
 
-# distribute ca
-mkdir -p /etc/kubernetes/ssl
-cp -rf ./*.pem /etc/kubernetes/ssl/
-for node in "${ALL_SERVER_IPS[@]}"
-do
-  ssh "${USER}@${node}" "mkdir -p /etc/kubernetes/ssl"
-  scp -r ./*.pem "${USER}@${node}:/etc/kubernetes/ssl/"
-done
+  # distribute ca
+  mkdir -p /etc/kubernetes/ssl
+  cp -rf ./*.pem /etc/kubernetes/ssl/
+  for node in "${ALL_SERVER_IPS[@]}"; do
+    ssh "${USER}@${node}" "mkdir -p /etc/kubernetes/ssl"
+    scp -r ./*.pem "${USER}@${node}:/etc/kubernetes/ssl/"
+  done
 
-## create TLS Bootstrapping Token
-tar -C ./tmp -zxvf ./bins/kubernetes-server-linux-amd64.tar.gz
-chmod +x ./tmp/kubernetes/server/bin/kubectl
-cp -f ./tmp/kubernetes/server/bin/kubectl /usr/local/bin/kubectl
+  ## create TLS Bootstrapping Token
+  tar -C ./tmp -zxvf ./bins/kubernetes-server-linux-amd64.tar.gz
+  chmod +x ./tmp/kubernetes/server/bin/kubectl
+  cp -f ./tmp/kubernetes/server/bin/kubectl /usr/local/bin/kubectl
 
-## set cluster params
-KUBE_APISERVER="https://${MASTER_IP}:6443"
-cd /etc/kubernetes || return
-kubectl config set-cluster kubernetes \
-  --certificate-authority=/etc/kubernetes/ssl/ca.pem \
-  --embed-certs=true \
-  --server="${KUBE_APISERVER}"
-kubectl config set-credentials admin \
-  --client-certificate=/etc/kubernetes/ssl/admin.pem \
-  --embed-certs=true \
-  --client-key=/etc/kubernetes/ssl/admin-key.pem
-kubectl config set-context kubernetes \
-  --cluster=kubernetes \
-  --user=admin
-kubectl config use-context kubernetes
+  ## set cluster params
+  KUBE_APISERVER="https://${MASTER_IP}:6443"
+  cd /etc/kubernetes || return
+  kubectl config set-cluster kubernetes \
+    --certificate-authority=/etc/kubernetes/ssl/ca.pem \
+    --embed-certs=true \
+    --server="${KUBE_APISERVER}"
+  kubectl config set-credentials admin \
+    --client-certificate=/etc/kubernetes/ssl/admin.pem \
+    --embed-certs=true \
+    --client-key=/etc/kubernetes/ssl/admin-key.pem
+  kubectl config set-context kubernetes \
+    --cluster=kubernetes \
+    --user=admin
+  kubectl config use-context kubernetes
 
-BOOTSTRAP_TOKEN=$(head -c 16 /dev/urandom | od -An -t x | tr -d ' ')
-export BOOTSTRAP_TOKEN
-cat > token.csv <<EOF
-${BOOTSTRAP_TOKEN},kubelet-bootstrap,10001,"system:kubelet-bootstrap"
-EOF
+  BOOTSTRAP_TOKEN=$(head -c 16 /dev/urandom | od -An -t x | tr -d ' ')
+  sed "s/BOOTSTRAP_TOKEN/${BOOTSTRAP_TOKEN}/" ./ssl/token.csv > ./tmp/token.csv
 
-## create kubelet bootstrapping kubeconfig
-cd /etc/kubernetes || return
-kubectl config set-cluster kubernetes \
-  --certificate-authority=/etc/kubernetes/ssl/ca.pem \
-  --embed-certs=true \
-  --server="${KUBE_APISERVER}" \
-  --kubeconfig=bootstrap.kubeconfig
-kubectl config set-credentials kubelet-bootstrap \
-  --token="${BOOTSTRAP_TOKEN}" \
-  --kubeconfig=bootstrap.kubeconfig
-kubectl config set-context default \
-  --cluster=kubernetes \
-  --user=kubelet-bootstrap \
-  --kubeconfig=bootstrap.kubeconfig
-kubectl config use-context default --kubeconfig=bootstrap.kubeconfig
+  ## create kubelet bootstrapping kubeconfig
+  cd /etc/kubernetes || return
+  kubectl config set-cluster kubernetes \
+    --certificate-authority=/etc/kubernetes/ssl/ca.pem \
+    --embed-certs=true \
+    --server="${KUBE_APISERVER}" \
+    --kubeconfig=bootstrap.kubeconfig
+  kubectl config set-credentials kubelet-bootstrap \
+    --token="${BOOTSTRAP_TOKEN}" \
+    --kubeconfig=bootstrap.kubeconfig
+  kubectl config set-context default \
+    --cluster=kubernetes \
+    --user=kubelet-bootstrap \
+    --kubeconfig=bootstrap.kubeconfig
+  kubectl config use-context default --kubeconfig=bootstrap.kubeconfig
 
-### create kube-proxy kubeconfig
-cd /etc/kubernetes || return
-kubectl config set-cluster kubernetes \
-  --certificate-authority=/etc/kubernetes/ssl/ca.pem \
-  --embed-certs=true \
-  --server="${KUBE_APISERVER}" \
-  --kubeconfig=kube-proxy.kubeconfig
-kubectl config set-credentials kubelet-proxy \
-  --client-certificate=/etc/kubernetes/ssl/kube-proxy.pem \
-  --client-key=/etc/kubernetes/ssl/kube-proxy-key.pem \
-  --embed-certs=true \
-  --kubeconfig=kube-proxy.kubeconfig
-kubectl config set-context default \
-  --cluster=kubernetes \
-  --user=kube-proxy \
-  --kubeconfig=kube-proxy.kubeconfig
-kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+  ### create kube-proxy kubeconfig
+  cd /etc/kubernetes || return
+  kubectl config set-cluster kubernetes \
+    --certificate-authority=/etc/kubernetes/ssl/ca.pem \
+    --embed-certs=true \
+    --server="${KUBE_APISERVER}" \
+    --kubeconfig=kube-proxy.kubeconfig
+  kubectl config set-credentials kubelet-proxy \
+    --client-certificate=/etc/kubernetes/ssl/kube-proxy.pem \
+    --client-key=/etc/kubernetes/ssl/kube-proxy-key.pem \
+    --embed-certs=true \
+    --kubeconfig=kube-proxy.kubeconfig
+  kubectl config set-context default \
+    --cluster=kubernetes \
+    --user=kube-proxy \
+    --kubeconfig=kube-proxy.kubeconfig
+  kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
 
-### distribute files
-for node in "${ALL_SERVER_IPS[@]}"
-do
-  scp token.csv bootstrap.kubeconfig kube-proxy.kubeconfig "${USER}@${node}:/etc/kubernetes/"
-done
+  ### distribute files
+  for node in "${ALL_SERVER_IPS[@]}"; do
+    scp ./tmp/token.csv bootstrap.kubeconfig kube-proxy.kubeconfig "${USER}@${node}:/etc/kubernetes/"
+  done
 
-# install etcd cluster
-cd "${BASE_PATH}" || return
-ETCD_IPS=("${ALL_SERVER_IPS[@]:0:3}")
-ETCD_ENDPOINTS="https://${ALL_SERVER_IPS[0]}:2379,https://${ALL_SERVER_IPS[1]}:2379,https://${ALL_SERVER_IPS[2]}:2379"
-ETCD_NODES="etcd-node0=https://${ALL_SERVER_IPS[0]}:2380,etcd-node1=https://${ALL_SERVER_IPS[1]}:2380,etcd-node2=https://${ALL_SERVER_IPS[2]}:2380"
+  # install etcd cluster
+  cd "${BASE_PATH}" || return
 
-# etcd cluster need cluster time sync
-# so we make an ntp server on the master node 
-# and node sync time according to master
-makeClusterTimeSync
+  # etcd cluster need cluster time sync
+  # so we make an ntp server on the master node 
+  # and node sync time according to master
+  makeClusterTimeSync
 
-# generate etcd env file
-for i in "${!ETCD_IPS[@]}"
-do
-order=$((i))
-cat > ./tmp/etcd-node"${order}".conf  << EOF
-ETCD_NAME=etcd-node${order}
-NODE_IP=${ETCD_IPS[$i]}
-ETCD_NODES="${ETCD_NODES}"
-EOF
-done
+  # generate etcd env file
+  for i in "${!ETCD_IPS[@]}"; do
+    order=$((i))
+    sed "s/ETCD_NAME_VAL/${order}/; s/NODE_IP_VAL/${ETCD_IPS[$i]}/; s/ETCD_NODES_VAL/${ETCD_NODES}/" ./systemd/etcd.conf > ./tmp/etcd-node"${order}".conf
+  done
 
-tar -C ./tmp -zxvf ./bins/etcd-v3.3.13-linux-amd64.tar.gz
-chmod +x ./tmp/etcd-v3.3.13-linux-amd64/etcd*
-chmod 644 ./systemd/etcd.service
-for i in "${!ETCD_IPS[@]}"
-do
-installETCD "${ETCD_IPS[$i]}"
-done
+  tar -C ./tmp -zxvf ./bins/etcd-v3.3.13-linux-amd64.tar.gz
+  chmod +x ./tmp/etcd-v3.3.13-linux-amd64/etcd*
+  chmod 644 ./systemd/etcd.service
+  for i in "${!ETCD_IPS[@]}"; do
+    installETCD "${ETCD_IPS[$i]}"
+  done
 
-sleep 10
-etcdctl \
-  --ca-file=/etc/kubernetes/ssl/ca.pem \
-  --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
-  --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
-  cluster-health
+  sleep 10
+  etcdctl \
+    --ca-file=/etc/kubernetes/ssl/ca.pem \
+    --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
+    --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
+    cluster-health
 
-printf "etcd cluster started! \n \n"
+  printf "etcd cluster started! \n \n"
 
-# install FLANNEL
-tar -C tmp -zxvf ./bins/flannel-v0.11.0-linux-amd64.tar.gz
-chmod u+x tmp/flanneld
-chmod u+x tmp/mk-docker-opts.sh
-cat > ./tmp/flanneld.conf <<EOF
-FLANNEL_ETCD_ENDPOINTS="${ETCD_ENDPOINTS}"
-FLANNEL_ETCD_PREFIX="/kube-centos/network"
-FLANNEL_OPTIONS="-etcd-cafile=/etc/kubernetes/ssl/ca.pem -etcd-certfile=/etc/kubernetes/ssl/kubernetes.pem -etcd-keyfile=/etc/kubernetes/ssl/kubernetes-key.pem --iface-regex=eth*|enp*"
-EOF
+  # install FLANNEL
+  tar -C tmp -zxvf ./bins/flannel-v0.11.0-linux-amd64.tar.gz
+  chmod u+x tmp/flanneld
+  chmod u+x tmp/mk-docker-opts.sh
+  sed "s@ETCD_ENDPOINTS_VAL@${ETCD_ENDPOINTS}@" ./systemd/flannel.conf > ./tmp/flannel.conf
 
-# register flannel subnet to etcd
-etcdctl --endpoints="${ETCD_ENDPOINTS}" \
-  --ca-file=/etc/kubernetes/ssl/ca.pem \
-  --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
-  --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
-  mkdir /kube-centos/network || true
-etcdctl --endpoints="${ETCD_ENDPOINTS}" \
-  --ca-file=/etc/kubernetes/ssl/ca.pem \
-  --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
-  --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
-  mk /kube-centos/network/config '{"Network":"172.30.0.0/16","SubnetLen":24,"Backend":{"Type":"vxlan"}}' || true
 
-for node in "${NODE_IPS[@]}"
-do
-  installFlannel "$node"
-done
+  # register flannel subnet to etcd
+  etcdctl --endpoints="${ETCD_ENDPOINTS}" \
+    --ca-file=/etc/kubernetes/ssl/ca.pem \
+    --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
+    --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
+    mkdir /kube-centos/network || true
+  etcdctl --endpoints="${ETCD_ENDPOINTS}" \
+    --ca-file=/etc/kubernetes/ssl/ca.pem \
+    --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
+    --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
+    mk /kube-centos/network/config '{"Network":"172.30.0.0/16","SubnetLen":24,"Backend":{"Type":"vxlan"}}' || true
 
-# ensure flanneld cluster started
-sleep 10
+  for node in "${NODE_IPS[@]}"; do
+    installFlannel "$node"
+  done
 
-# output flanneld subnet
-etcdctl --endpoints="${ETCD_ENDPOINTS}" \
-  --ca-file=/etc/kubernetes/ssl/ca.pem \
-  --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
-  --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
-  ls /kube-centos/network/subnets
+  # ensure flanneld cluster started
+  sleep 5
 
-# install DOCKER
-cd "${BASE_PATH}" || return
-tar -C ./tmp -zxvf ./bins/docker-18.09.6.tgz
-cat > ./tmp/daemon.json << EOF
-{
-  "graph": "${DOCKER_LOCATION}"
+  # output flanneld subnet
+  etcdctl --endpoints="${ETCD_ENDPOINTS}" \
+    --ca-file=/etc/kubernetes/ssl/ca.pem \
+    --cert-file=/etc/kubernetes/ssl/kubernetes.pem \
+    --key-file=/etc/kubernetes/ssl/kubernetes-key.pem \
+    ls /kube-centos/network/subnets
+
+  # install DOCKER
+  cd "${BASE_PATH}" || return
+  tar -C ./tmp -zxvf ./bins/docker-18.09.6.tgz
+  sed "s/DOCKER_LOCATION/${DOCKER_LOCATION}/" ./systemd/docker-daemon.json > ./tmp/daemon.json
+
+  for node in "${ALL_SERVER_IPS[@]}"; do
+    installDocker "$node"
+  done
+
+
+  # install K8S components in master
+  installKubernetesMaster
+  # give kubelet-bootstrap user system:node-bootstrapper cluster role
+  # so kubelet can create certificate signing requests
+  kubectl delete clusterrolebinding kubelet-bootstrap || true
+  kubectl create clusterrolebinding kubelet-bootstrap \
+    --clusterrole=system:node-bootstrapper \
+    --user=kubelet-bootstrap || true
+  kubectl get componentstatuses
+
+
+  ## install k8s components in every node
+  for node in "${NODE_IPS[@]}"; do
+    installKubernetsNode "${node}"
+  done
 }
-EOF
 
-for node in "${ALL_SERVER_IPS[@]}"
-do
-  installDocker "$node"
+
+# get system params to init some env
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -s|--system) HOST_SYSTEM="$2"
+    shift
+    shift
+    ;;
+    *) shift
+    ;;
+  esac
+  
 done
 
+## default host system is centos
+SERVICE_UNIT_LOCATION="/usr/lib/systemd/system"
+if [ "$HOST_SYSTEM" == "ubuntu" ]; then
+  SERVICE_UNIT_LOCATION="/lib/systemd/system"
+fi
 
-# install K8S components in master
-installKubernetesMaster
-# give kubelet-bootstrap user system:node-bootstrapper cluster role
-# so kubelet can create certificate signing requests
-kubectl delete clusterrolebinding kubelet-bootstrap || true
-kubectl create clusterrolebinding kubelet-bootstrap \
-  --clusterrole=system:node-bootstrapper \
-  --user=kubelet-bootstrap || true
-kubectl get componentstatuses
-
-
-## install k8s components in every node
-for node in "${NODE_IPS[@]}"; do
-  installKubernetsNode "${node}"
-done
+# start main install process
+main
